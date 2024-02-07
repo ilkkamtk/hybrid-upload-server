5eca0bf268b27dc199fd9e8dd2a57b89d00c5de0
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-02-08 00:20:30 +0200

    ffmpeg debug

--- src/utils/getVideoThumbnail.ts
+++ src/utils/getVideoThumbnail.ts
@@ -3,13 +3,17 @@ import ffmpeg from 'fluent-ffmpeg';
 const getVideoThumbnail = (videoUrl: string) => {
   // resolves always because even if there is an error, ffmpeg will still create a thumbnail
   return new Promise((resolve) => {
-    ffmpeg(videoUrl)
+    ffmpeg()
+      .input(videoUrl)
       .on('end', () => {
         resolve(true);
       })
       .screenshots({
         count: 1,
         filename: './uploads/%b-thumb.png',
+      })
+      .on('error', (error) => {
+        console.error('ffmpeg', error);
       });
   });
 };
