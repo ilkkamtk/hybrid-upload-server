9734eea5f2757235933a75379f5d711d9071cdc4
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-05-23 17:34:18 +0300

    dix

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
 import app from './app';
-import https from 'https';
-import fs from 'fs';
-
-if (process.env.NODE_ENV === 'production') {
-  const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
-  const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
-
-  const options = {
-    key: sslkey,
-    cert: sslcert,
-  };
-  const httpsPort = process.env.HTTPS_PORT || 8002;
-  https.createServer(options, app).listen(httpsPort);
-}
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
