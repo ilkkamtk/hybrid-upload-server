7032ea512044f2c43cdbbb8ce3f6360fc7346428
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2025-02-13 02:31:47 +0200

    refactor: update file upload process to retrieve user ID from res.locals instead of request body

--- src/api/routes/fileRoute.ts
+++ src/api/routes/fileRoute.ts
@@ -1,14 +1,15 @@
 import express, {NextFunction, Request, Response} from 'express';
 import {deleteFile, uploadFile} from '../controllers/uploadController';
 import multer from 'multer';
-import {addUserToBody, authenticate, makeThumbnail} from '../../middlewares';
+import {authenticate, makeThumbnail} from '../../middlewares';
 import CustomError from '../../classes/CustomError';
 import {TokenContent} from 'hybrid-types/DBTypes';
 
 const storage = multer.diskStorage({
   destination: './uploads/',
   filename: (req, file, cb) => {
-    const userId = req.body.user.user_id;
+    // Get user from res.locals instead of req.body
+    const userId = (req as Request).res?.locals.user.user_id;
     const extension = file.originalname.split('.').pop();
     const newFilename = `${file.fieldname}_${userId}.${extension}`;
     cb(null, newFilename);
@@ -22,7 +23,6 @@ const doUpload = (
   res: Response<unknown, {user: TokenContent}>,
   next: NextFunction,
 ) => {
-  console.log('doUpload', req.body);
   upload(req, res, (err) => {
     if (err) {
       next(new CustomError(err.message, 400));
@@ -41,9 +41,7 @@ const doUpload = (
 
 const router = express.Router();
 
-router
-  .route('/upload')
-  .post(authenticate, addUserToBody, doUpload, makeThumbnail, uploadFile);
+router.route('/upload').post(authenticate, doUpload, makeThumbnail, uploadFile);
 
 router.route('/delete/:filename').delete(authenticate, deleteFile);
 
